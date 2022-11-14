54858b7c7c85bbf75672314ce0d711365604c4da
Candace Eckels <candace.eckels@gmail.com>
2022-11-14 13:09:58 -0700

    Finished troubleshooting of the serializer

--- app/serializers/tourist_sights_serializer.rb
+++ app/serializers/tourist_sights_serializer.rb
@@ -2,16 +2,16 @@ class TouristSightsSerializer
 
   def self.sights(sights)
     { data: 
-      { sights.map do |sight|
-        id: nil,
+      sights.map do |sight|
+       { id: nil,
         type: sight.class.to_s.downcase,
           attributes: {
             name: sight.name,
             address: sight.address,
             place_id: sight.place_id
           }
-        end
-      }
-      }
+        }
+      end
+    }
   end
 end
