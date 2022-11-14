e6f2e351b7dde50ff1299432ba55a142c40a14da
Candace Eckels <candace.eckels@gmail.com>
2022-11-14 12:50:40 -0700

    sight poro copmlete

--- app/facades/places_facade.rb
+++ app/facades/places_facade.rb
@@ -3,10 +3,10 @@ class PlacesFacade
   def self.find_sights(country)
     country_data = RestCountryService.search_country_by_name(country).first
     country_object = Country.new(country_data)
-    country_sights = PlacesService.find_sights(country_object.capital_long_lat[0], country_object.capital_long_lat[1])
-    country_sights.map do |sight_data|
+    country_sights = PlacesService.find_sights(country_object.capital_long_lat[0], country_object.capital_long_lat[1])[:features]
+    sights = country_sights.map do |sight_data|
       Sight.new(sight_data)
-      binding.pry
     end
+    sights
   end
 end
