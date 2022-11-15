86bef7e514b90d64c274f0b8d5ca7c2a3844df4c
Candace Eckels <candace.eckels@gmail.com>
2022-11-15 11:02:27 -0700

    Connected to youtube service

--- app/facades/learning_resources_facade.rb
+++ app/facades/learning_resources_facade.rb
@@ -1,7 +1,8 @@
 class LearningResourcesFacade
 
-  def acquire_resources(country)
+  def self.acquire_resources(country)
     video_data = YoutubeService.video_data(country)
+    video_object = Video.new(video_data)
     
   end
 
---
+++ app/poros/video.rb
@@ -0,0 +1,8 @@
+class Video
+  attr_reader :title, :youtube_video_id
+
+  def initialize(data)
+    @title = data[:items].first[:snippet][:title]
+    @youtube_video_id = data[:items].first[:id][:videoId]
+  end
+end
