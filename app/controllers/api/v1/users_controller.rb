54e573f319d2b52685747b85c187dc116aafc321
Candace Eckels <candace.eckels@gmail.com>
2022-11-15 16:55:40 -0700

    Happy path testing complete for User CRUD

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,6 +9,37 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def show
+    user = User.find(params[:id])
+    render json: UserSerializer.format_user(user)
+  end
+
+  def index
+    users = User.all
+    render json: UserSerializer.format_all(users)
+  end
+
+  def update
+    if User.exists?(params[:id])
+      user = User.find(params[:id])
+      if user.update(user_params)
+        render json: UserSerializer.format_user(user), status: 202
+      else
+        render json: { error: 'Unsuccessful update' }, status: 404
+      end
+    else
+      render json: { error: 'User Not Found' }, status: 404
+    end
+  end
+
+  def destroy
+    if User.exists?(params[:id])
+      User.destroy(params[:id])
+    else
+      render json: { error: 'No User Found' }, status: 404
+    end
+  end
+
   private
 
   def user_params
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -14,4 +14,21 @@ class UserSerializer
     }
   end
 
+  def self.format_all(users)
+    {
+      data: 
+      users.map do |user|
+        {
+          type: user.class.to_s.downcase,
+          id: user.id.to_s,
+          attributes: {
+            name: user.name,
+            email: user.email,
+            api_key: user.api_key
+          }
+        }
+        end
+    }
+  end
+
 end
